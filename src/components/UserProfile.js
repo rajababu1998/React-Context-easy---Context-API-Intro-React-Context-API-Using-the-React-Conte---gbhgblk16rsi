1da6dbdfcbbe6ae8e2bc46ca18fa9925b159840a
Raja Babu <50585246+rajababu1998@users.noreply.github.com>
2022-09-15 10:55:53 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,7 +3,13 @@ import React from 'react'
 import { UserContext } from "./App"
 
 const UserProfile = () =>{
-
+  const localVar = useContext(UserContext);
+  return (
+    <>
+      <h1 id="name">Name:-{localVar.name}</h1>
+      <h1 id="age">Age:-{localVar.age}</h1>
+    </>
+  );
 }
 
 export {UserProfile}
